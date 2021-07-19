e9b1e0fe6c3f432aed22e3dd3059903764b5c60e
Kdoni <63546827+Kdoni@users.noreply.github.com>
2021-07-19 10:46:10 +0100

    initial commit

---
+++ src/main/kotlin/Week 7 Assessment.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var num=267239
+    println("Number: $num")
+    println("Reverse number: " + reverseNumber(num))
+
+}
+fun reverseNumber(number:Int):Int{
+    var number=number
+    var reverse=0
+    while (number!=0){
+        reverse=reverse*10+number%10
+        number= number/10
+    }
+    return reverse
+
+}
